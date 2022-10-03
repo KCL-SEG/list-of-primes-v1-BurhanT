076cb1e02b30af8bd7f159a9fb106b8c720c83e4
Burhan Tekcan <burhan.tekcan@kcl.ac.uk>
2022-10-03 02:48:44 +0100

    fix mistake 2

--- primes.py
+++ primes.py
@@ -4,11 +4,11 @@
 def primes(number_of_primes):
     list = [2]
     i = 1
-    while i <= (number_of_primes + 1):
+    while len(list) < number_of_primes:
         for j in range(2, i):
             if (i % j == 0):
                 break
-            else:
+            elif i not in list:
                 list.append(i)
         i += 1
     return list
