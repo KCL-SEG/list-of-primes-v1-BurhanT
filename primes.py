3cb550c248449ba19e6941ad8c99edbd91601388
Burhan Tekcan <burhan.tekcan@kcl.ac.uk>
2022-10-03 02:24:45 +0100

    remove +1 from while loop

--- primes.py
+++ primes.py
@@ -4,7 +4,7 @@
 def primes(number_of_primes):
     list = [2]
     i = 1
-    while i <= (number_of_primes + 1):
+    while i <= (number_of_primes):
         for j in range(2, i):
             if (i % j == 0):
                 break
