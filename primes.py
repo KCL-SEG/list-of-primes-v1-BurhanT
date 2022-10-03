e61735cec496d2b27a990a4d3a8955915d2e48bf
Burhan Tekcan <burhan.tekcan@kcl.ac.uk>
2022-10-03 02:17:15 +0100

    list primes up to the number of primes wanted #2

--- primes.py
+++ primes.py
@@ -4,7 +4,7 @@
 def primes(number_of_primes):
     list = []
     i = 1
-    while i < (number_of_primes + 1):
+    while i <= (number_of_primes + 1):
         for j in range(2, i):
             if (i % j == 0):
                 break
