3b6acccf6ad55bf62c1c9f0c408e385423eb0603
Burhan Tekcan <burhan.tekcan@kcl.ac.uk>
2022-10-03 02:08:54 +0100

    list primes from 1 to value #3

--- primes.py
+++ primes.py
@@ -3,8 +3,8 @@
 
 def primes(number_of_primes):
     list = []
-    for i in range(1, number_of_primes):
-        for j in range(2, i):
+    for i in range(0, number_of_primes):
+        for j in range(1, i):
             if (i % j == 0):
                 break
             else:
